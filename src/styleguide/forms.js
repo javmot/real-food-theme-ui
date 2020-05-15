b4557f1c23de1053112dd0864fea4b6c096d5147
Javier Mota <javimota83@gmail.com>
2020-05-15 12:30:41 +0200

    Remove defaultValues in forms

--- src/styleguide/forms.js
+++ src/styleguide/forms.js
@@ -20,28 +20,17 @@ export default () => (
     </Flex>
     <Box sx={{ width: ["full", "full", 8, 5] }}>
       <Styled.h2>Form Fields</Styled.h2>
-      <MaterialField label="Material" name="material" defaultValue="" />
+      <MaterialField label="Material" name="material" />
       <Grid gap={[3, 3, 3, 7]} py={5} columns={2}>
-        <Field label="Email" name="email2" defaultValue="" />
-        <Field
-          as={Select}
-          label="Greeting"
-          name="greeting"
-          defaultValue="Hello"
-        >
+        <Field label="Email" name="email2" />
+        <Field as={Select} label="Greeting" name="greeting">
           <option>Hello</option>
           <option>Hi</option>
           <option>Beep</option>
           <option>Boop</option>
         </Field>
       </Grid>
-      <Field
-        as={Textarea}
-        label="Description"
-        rows={8}
-        name="description"
-        defaultValue="Hello"
-      />
+      <Field as={Textarea} label="Description" rows={8} name="description" />
       <Button mt={4}>Submit</Button>
       <Styled.h2>Status</Styled.h2>
       <Field label="Focus" name="error" status="focus" />
